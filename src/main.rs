eb7e61c4bad4e3aa1bb9c2365847f818c67c935b
Hao Shi <shisoftgenius@gmail.com>
2018-01-30 00:32:19 +0800

    timing

--- src/main.rs
+++ src/main.rs
@@ -36,6 +36,7 @@ struct ExitStatus {
     statm: Statm,
     stdout: String,
     stderr: String,
+    time: i64,
     code: i32
 }
 
@@ -101,7 +102,7 @@ fn main() {
                 cmd.arg(arg);
             }
         }
-        let watch = Stopwatch::start_new();
+        let mut watch = Stopwatch::start_new();
         let child = {
             match cmd
                 .stdout(Stdio::piped())
@@ -144,11 +145,13 @@ fn main() {
             });
         }
         let output = child.wait_with_output().unwrap();
+        watch.stop();
         let term_sender = wrapped_sender.lock();
         term_sender.send(TerminationState::Exited(ExitStatus {
             statm: unsafe { *max_stat_clone.load(Ordering::Relaxed) },
             stdout: String::from_utf8(output.stdout).unwrap(),
             stderr: String::from_utf8(output.stderr).unwrap(),
+            time: watch.elapsed_ms(),
             code: output.status.code().unwrap()
         }));
     });
